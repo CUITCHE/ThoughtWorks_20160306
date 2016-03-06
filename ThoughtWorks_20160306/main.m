96192597abff35b7032fc26603c227af9a687279
He Junqiu <xiaohe918@vip.qq.com>
2016-03-06 10:11:20 +0800

    Initial Commit

---
+++ ThoughtWorks_20160306/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThoughtWorks_20160306
+//
+//  Created by hejunqiu on 16/3/6.
+//  Copyright © 2016年 hejunqiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
